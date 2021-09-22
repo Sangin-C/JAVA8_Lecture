70c2a4c490c5d9e12a07903bb9b2f58a578c6eaa
Sangin-C <sangin3435@naver.com>
2021-09-22 22:46:15 +0900

    챕터1. 함수형 인터페이스와 람다 - 함수형 인터페이스와 람다 표현식 소개

---
+++ src/main/java/com/locky/java8to11/Foo.java
@@ -0,0 +1,9 @@
+package com.locky.java8to11;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        int baseNumber = 10;
+        RunSomething runSomething = number -> baseNumber + 10;
+    }
+}
---
+++ src/main/java/com/locky/java8to11/RunSomething.java
@@ -0,0 +1,8 @@
+package com.locky.java8to11;
+
+@FunctionalInterface
+public interface RunSomething {
+
+    int doIt(int number);
+
+}
